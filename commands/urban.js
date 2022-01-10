d8073c46319c646b6f0c9cad23007878ef9e013c
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-01-10 12:14:52 -0500

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -3,39 +3,29 @@ const axios = require('axios');
 
 module.exports = {
 	name: 'urban',
-	cooldown: 3000,
 	run: async (client, message, args) => {
-
 		const query = args.join(' ');
 		if (!query) {
 			return message.channel.send(`${client.emotes.error} | Please provide a query`);
 		}
-		const link = 'https://api.urbandictionary.com/v0/define?term=';
+		const link = 'https://api.urbandictionary.com/v0/define?';
 		let fetch = await axios(link + encodeURI(query));
 		fetch = fetch.data.list;
 
 		if (fetch.length === 0) {
-			return message.channel.send(`${client.emotes.error} | Query not found`);
+			return message.channel.send(`${client.emotes.error} | No results found for **${query}**.`);
 		}
 		const data = fetch[0];
-		let definition = data.definition;
-		let example = data.example;
-		const permalink = data.permalink;
-		const thumbsUp = data.thumbs_up;
-		const thumbsDown = data.thumbs_down;
-		const title = data.word;
-
-		definition = definition.length >= 1024 ? definition.slice(0, 1024) + '...' : definition;
-		example = example.length >= 1024 ? example.slice(0, 1024) + '...' : example;
 
 		const embed = new discord.MessageEmbed()
-			.setTitle(title)
-			.setURL(permalink)
-			.setDescription('Definition: ', definition)
-			.setDescription('Example: ', example)
-			.setColor('#DD989B')
-			.setFooter({ text: `👍: ${thumbsUp} | 👎: ${thumbsDown}`, iconURL: 'https://th.bing.com/th/id/OIP.vUVYShzId3cwgBrxhl9eeAHaHa?pid=ImgDet&rs=1' });
-
-		return message.channel.send(embed);
+			.setColor('#EFFF00')
+			.setTitle(data.word)
+			.setURL(data.permalink)
+			.addFields(
+				{ name: 'Definition', value: (data.definition) },
+				{ name: 'Example', value: (data.example) },
+				{ name: 'Rating', value: `${data.thumbs_up} thumbs up. ${data.thumbs_down} thumbs down.` },
+			);
+		message.channel.send({ embeds: [embed] });
 	},
 };
