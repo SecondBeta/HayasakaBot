a2d6bb535e20019efcc69faaa194db3c9827f544
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-13 23:32:53 -0400

    please...

--- commands/purge.js
+++ commands/purge.js
@@ -1,26 +1,30 @@
 const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports = {
 	name: 'purge',
 	run: async (client, message, args) => {
-		const purgeCount = Number(args[0]);
-		if (!purgeCount) {
-			return message.channel.send(`${client.emotes.error} | Please provide a # of previous messages you would like to delete (.purge 50)`);
-		}
-		if (purgeCount > 100 || purgeCount < 1) {
-		return message.channel.send(`${client.emotes.error} | Please select a number **between** 1 and 100`)
-		}
-		if (isNaN(purgeCount)) return message.channel.send(`${client.emotes.error} | Please enter a valid number!`);
-		await message.channel.messages.fetch({
-			limit: purgeCount
-		  }).then(messages => {
-			message.channel.bulkDelete(messages);
+		client.config = require('../config.json');
+		if(message.member.roles.cache.has(config.modRoleID)){
+			const purgeCount = Number(args[0]);
+			if (!purgeCount) {
+				return message.channel.send(`${client.emotes.error} | Please provide a # of previous messages you would like to delete (.purge 50)`);
+			}
+			if (purgeCount > 100 || purgeCount < 1) {
+			return message.channel.send(`${client.emotes.error} | Please select a number **between** \`1\` and \`100\``)
+			}
+			message.channel.messages.channel.bulkDelete(purgeCount);
 			message.channel.send({
 				embeds: [
 					new Discord.MessageEmbed()
 						.setTitle(`Purged ${purgeCount} messages!`)
 						.setImage('https://cdn.discordapp.com/attachments/929954012097310731/963989793660895262/693990042485522432.gif')
-				]});
-		  })
+				]}).then((msg) => {
+					msg.react(`${client.emotes.success}`);
+					setTimeout(() => {
+						msg.delete();
+					}, 5000);
+				});
+		}
 	},
 };
