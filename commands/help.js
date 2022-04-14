50baaaf2ae937f32123088f36799b087eb5a6b61
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-13 22:55:48 -0400

    Import error resolved

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const config = require('../config.json');
 
-client.config = require('./config.json');
+client.config = require('../config.json');
 
 module.exports = {
 	name: 'help',
