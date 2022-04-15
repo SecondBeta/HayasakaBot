0998446fff339f91c2e96aabc95d34752b740db2
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-14 23:01:55 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -10,34 +10,14 @@ module.exports = {
         if (!args[0]) {
             return message.channel.send(`${client.emotes.error} | Please put code to evaluate in code blocks: \`\`\`\ console.log('hello there') \`\`\`\ `)
         }
-        const embed = new MessageEmbed()
-            .setTitle('Evaluating...')
-            .setColor(config.embedColour)
-        const msg = await message.channel.send({ embeds: [embed] });
         try {
             const data = eval(args.join(' ').replace(/```/g, ''))
             const embed = new MessageEmbed()
                 .setTitle('Evaluated Code')
                 .setDescription(await `${data}`)
                 .setColor(config.embedColour)
-            await msg.edit(embed);
-            await msg.react('✅')
-            await msg.react('❌')
-            const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id)
-            msg.awaitReactions(filter, { max: 1 })
-                .then((collected) => {
-                    collected.map((emojis) => {
-                        switch (emojis._emoji.name) {
-                            case '✅':
-                                msg.reactions.removeAll()
-                                break;
-                            case '❌':
-                                msg.delete()
-                                break;
-                        }
-                    })
-                })
-
+                
+                await message.channel.send({ embeds: [embed] });
         } catch (error) {
             const embed = new MessageEmbed()
                 .setTitle('An Error Occurred')
