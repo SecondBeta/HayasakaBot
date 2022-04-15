bc71a3b778c994894c50ef86cd52fab3d5fed9ae
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-14 23:49:39 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,13 +4,14 @@ module.exports = {
     name: 'eval',
     run: async (client, message, args) => {
         client.config = require('../config.json');
-        if (!args) {
+        const code = args[0]
+        if (!code) {
             return message.channel.send(`${client.emotes.error} | Please provide code to evaluate`);
         }
         if (message.author.id !== config.devID)
         return; 
         try {
-            const evaled = eval(args.join(' '));
+            const evaled = eval(code.join(' ').replace(/```/g, ''))
             await message.channel.send(`\`\`\`js\n${evaled}\n\`\`\``);
         } catch (err) {
             message.channel.send(`\`ERROR\` \`\`\`xl\n${evaled}\n\`\`\``);
