3d4120af6a4d50bde96901cec7327720e7c367e6
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-14 23:31:30 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,16 +4,15 @@ module.exports = {
     name: 'eval',
     run: async (client, message, args) => {
         client.config = require('../config.json');
-    const args = message.content.split(" ").slice(1);
-      if (message.author.id !== `${process.env.devID}`)
-        return;
-      try {
-        const evaled = eval(args.join(" "));
-        const cleaned = await clean(evaled);
-        message.channel.send(`\`\`\`js\n${cleaned}\n\`\`\``);
-
-      } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${cleaned}\n\`\`\``);
-      }
+        const args = message.content.split(" ").slice(1);
+        if (message.author.id !== config.devID)
+        return; 
+        try {
+            const evaled = eval(args.join(" "));
+            const cleaned = await clean(evaled);
+            message.channel.send(`\`\`\`js\n${cleaned}\n\`\`\``);
+        } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${cleaned}\n\`\`\``);
+        }
     }
 }
